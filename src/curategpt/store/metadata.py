9d021d542ba223cf422c6b8f8626ed99eeb0e602
iQuxLE <ckroll95@gmail.com>
2024-10-14 16:17:35 +0100

    making old CollectionMetadata available

--- src/curategpt/store/metadata.py
+++ src/curategpt/store/metadata.py
@@ -1,8 +1,42 @@
 import json
 from typing import Dict, Optional
 
-from pydantic import ConfigDict
+from pydantic import ConfigDict, BaseModel
 from venomx.model.venomx import Index
+
+class CollectionMetadata(BaseModel):
+    """
+    Metadata about a collection.
+    Useful for all even when no uniqye ID in objects. (e.g. MaxO Ontology)
+    This is an open class, so additional metadata can be added.
+    """
+
+    model_config = ConfigDict(protected_namespaces=())
+
+    name: Optional[str] = None
+    """Name of the collection"""
+
+    description: Optional[str] = None
+    """Description of the collection"""
+
+    model: Optional[str] = None
+    """Name of any ML model"""
+
+    object_type: Optional[str] = None
+    """Type of object in the collection"""
+
+    source: Optional[str] = None
+    """Source of the collection"""
+
+    # DEPRECATED
+    annotations: Optional[Dict] = None
+    """Additional metadata"""
+
+    object_count: Optional[int] = None
+    """Number of objects in the collection"""
+
+    hnsw_space: Optional[str] = None
+    """Space used for hnsw index (e.g. 'cosine')"""
 
 """
 ChromaDB Constraints:
